f113f53d4dc3c2148475639c1c2cd83fa3fa32b1
Alishia Subramoney <subramoneyalishia@gmail.com>
2022-10-26 22:19:30 +0200

    city

---
+++ models/city.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines City class."""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """Represent a city.
+    Attributes:
+        state_id (str): State id.
+        name (str): Name of the city.
+    """
+
+    state_id = ""
+    name = ""
