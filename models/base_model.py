d1f8e0c63b41e71300941f2c5d4893e156ee20a6
Alishia Subramoney <subramoneyalishia@gmail.com>
2022-10-26 22:10:48 +0200

    Base model

--- models/base_model.py
+++ models/base_model.py
@@ -25,15 +25,12 @@ class BaseModel:
             models.storage.new(self)
 
     def save(self):
-        """Update updated_at with the current datetime."""
+        """Update updated_at with the current datetime"""
         self.updated_at = datetime.today()
         models.storage.save()
 
     def to_dict(self):
-        """Return the dictionary of the BaseModel instance.
-        Includes the key/value pair __class__ representing
-        the class name of the object.
-        """
+        """Return the dictionary of the BaseModel instance"""
         rdict = self.__dict__.copy()
         rdict["created_at"] = self.created_at.isoformat()
         rdict["updated_at"] = self.updated_at.isoformat()
@@ -41,6 +38,6 @@ class BaseModel:
         return rdict
 
     def __str__(self):
-        """Return the print/str representation of the BaseModel instance."""
+        """Return the print/str representation of the BaseModel instance"""
         clname = self.__class__.__name__
         return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
